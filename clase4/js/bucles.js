7d6d42c992ef3094c7ade26e134df6f41c9219ec
franyvalde <franyvalde@gmail.com>
2022-03-16 14:39:23 -0300

    first commit

---
+++ clase4/js/bucles.js
@@ -0,0 +1,41 @@
+//ubicamos elementos dentro del DOM
+var txt = document.getElementById('txt');
+var parrafo = document.getElementById('parrafo');
+var lista = document.getElementById('lista');
+var lista2 = document.getElementById('lista2');
+
+/* colecciones de datos */
+var marcas = [
+                'LG', 'Motorola', 'Xiaomi',
+                'Huawei', 'Samsung', 'Alcatel'
+             ];
+txt.innerText = marcas[2];
+var italianos = [
+                    'Maseratti', 'Pagani', 'Ferrari',
+                    'Alfa Romeo', 'Lancia', 'Lamborghini',
+                    'Abarth', 'Fornasari'
+                ];
+
+// bucle while
+var n = 1;
+parrafo.innerHTML = '';
+while ( n < 10 ) {
+    parrafo.innerHTML = parrafo.innerHTML + n +'<br>';
+    n++; //n = n+1
+}
+
+// mostrando items de lista
+n = 0;
+var cantidad = marcas.length;
+lista.innerHTML = '';
+while( n < cantidad ){
+    lista.innerHTML = lista.innerHTML + '<li>'+marcas[n]+'</li>';
+    n++;
+}
+
+// autos italianos
+var cantidad = italianos.length;
+lista2.innerHTML = '';
+for ( n=0; n < cantidad; n++ ){
+    lista2.innerHTML = lista2.innerHTML +'<li>'+ italianos[n]+'</li>';
+}
---
+++ clase4/js/muestraColores.js
@@ -0,0 +1,25 @@
+//ubicamos elemento dentro del DOM
+var muestras = document.getElementById('muestras');
+
+//colecciones de colores
+var colores = [
+                'rojo', 'naranja', 'verde',
+                'rosa', 'azul', 'dorado',
+                'amarillo', 'violeta', 'turquesa'
+               ];
+var codigos = [
+                '#d00', '#f60', '#0d0',
+                '#e99', '#00d', '#DAA520',
+                '#ff0', '#e3e', '#40E0D0'
+              ];
+
+//generación de HTML
+var cantidad = colores.length;
+muestras.innerHTML = '';
+for ( var n=0; n<cantidad; n++ ) {
+    muestras.innerHTML += '<i style="background-color: '+codigos[n]+'"></i>' +colores[n]+ '<br>';
+}
+
+/*
+<i style="background-color: #ccc"></i> nombre de color <br>
+* */
